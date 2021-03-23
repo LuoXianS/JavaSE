7bbbd9b15329debd1c97007f797d22ef49992959
LuoXianS <2320418206@qq.com>
2021-03-23 20:26:42 +0800

    easy

--- MyArrayList/src/TestDemo.java
+++ MyArrayList/src/TestDemo.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class TestDemo {
     public static void main(String[] args) {
-
+        MyArrayList  list = new MyArrayList();
+        list.add(0,1);
+        list.add(1,2);
+        list.add(2,3);
+        list.add(3,4);
+        list.display();
+        list.add1(32);
+        list.add1(32);
+        list.display();
+        list.contains(65);
+        //list.search(4);
     }
 }
